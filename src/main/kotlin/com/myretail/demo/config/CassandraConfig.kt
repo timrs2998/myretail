e38d94fc45b9dc5c9a9bcbea26c39311328665d8
timrs2998 <timrs2998@gmail.com>
2017-05-19 22:24:37 -0500

    Add gradle wrapper, refactor cassandra config

--- src/main/kotlin/com/myretail/demo/config/CassandraConfig.kt
+++ src/main/kotlin/com/myretail/demo/config/CassandraConfig.kt
@@ -1,30 +1,20 @@
 package com.myretail.demo.config
 
 import org.springframework.cassandra.core.keyspace.CreateKeyspaceSpecification
-import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.data.cassandra.config.CassandraClusterFactoryBean
 import org.springframework.data.cassandra.config.java.AbstractCassandraConfiguration
-import org.springframework.data.cassandra.mapping.BasicCassandraMappingContext
-import org.springframework.data.cassandra.mapping.CassandraMappingContext
+import org.springframework.data.cassandra.repository.config.EnableCassandraRepositories
 
 @Configuration
-open class CassandraConfig : AbstractCassandraConfiguration() {
+@EnableCassandraRepositories(basePackages = arrayOf("com.myretail.demo.repository"))
+class CassandraConfig : AbstractCassandraConfiguration() {
 
-    override fun getKeyspaceName(): String = "testKeySpace"
+    override fun getKeyspaceName(): String = "myretail"
 
-    @Bean
-    override fun cluster(): CassandraClusterFactoryBean {
-        val cluster = CassandraClusterFactoryBean()
-        cluster.setContactPoints("127.0.0.1")
-        cluster.setPort(9142)
-        cluster.keyspaceCreations = listOf(CreateKeyspaceSpecification(keyspaceName))
-        return cluster
-    }
+    override fun getPort(): Int = 9142
 
-    @Bean
-    override fun cassandraMapping(): CassandraMappingContext {
-        return BasicCassandraMappingContext()
-    }
+    override fun getKeyspaceCreations() = listOf(
+            CreateKeyspaceSpecification.createKeyspace(keyspaceName)
+    )
 
 }
